4252bdb8c7fddf10b9a15e680b564fa6ce20634e
Chutchev <iChutchev98@gmail.com>
2018-11-03 20:46:24 +0500

    Переработана проверка на выигрыш

--- TicTac.py
+++ TicTac.py
@@ -3,8 +3,8 @@ import random
 
 def player_choose():
     player = input("За кого вы будете играть?\n")
-    if player.lower() != 'x' and player.lower() != 'o' and player.lower() != 'х' and player.lower() != 'о':
-        player_choose()
+    while player.lower() != 'x' and player.lower() != 'o' and player.lower() != 'х' and player.lower() != 'о':
+        player = input("За кого вы будете играть?\n")
     if player.lower() == 'x':
         comp = 'o'
     else:
@@ -31,7 +31,7 @@ def player_move(game_field, moves_list, player):
         else:
             error = True
     if error == True:
-        print('Вы не можете сходить в эту клетку. Она уже занята!')
+        print('Вы не можете сходить в эту клетку. Она уже занята или ее не существует!')
         print_game_field(game_field)
         player_move(game_field, moves_list, player)
 
@@ -51,27 +51,20 @@ def print_game_field(game_field):
 
 
 def check_winner(game_field):
-    if game_field[0] == game_field[1] and game_field[1] == game_field[2]:
-        return False
-    elif game_field[0] == game_field[4] and game_field[0] == game_field[8]:
-        return False
-    elif game_field[0] == game_field[3] and game_field[0] == game_field[6]:
-        return False
-    elif game_field[3] == game_field[4] and game_field[3] == game_field[5]:
-        return  False
-    elif game_field[6] == game_field[7] and game_field[6] == game_field[8]:
-        return False
-    elif game_field[1] == game_field[4] and game_field[1] == game_field[7]:
-        return False
-    elif game_field[2] == game_field[5] and game_field[2] == game_field[8]:
-        return False
+    continue_game = True
+    for index in range(3):
+        if game_field[index] == game_field[index+1] and game_field[index] == game_field[index + 2]:
+            continue_game = False
+        elif game_field[index] == game_field[index + 3] and game_field[index] == game_field[index + 6]:
+            continue_game = False
+    if game_field[0] == game_field[4] and game_field[0] == game_field[8]:
+        continue_game = False
     elif game_field[2] == game_field[4] and game_field[2] == game_field[6]:
-        return False
-    else:
-        return True
+        continue_game = False
+    return continue_game
 
 
-def first_player(game_field, moves_list, player, comp):
+def first_player_move(game_field, moves_list, player, comp):
     while True:
         print_game_field(game_field)
         if len(moves_list) > 0 and check_winner(game_field):
@@ -88,7 +81,7 @@ def first_player(game_field, moves_list, player, comp):
             break
 
 
-def first_comp(game_field, moves_list, player, comp):
+def first_comp_move(game_field, moves_list, player, comp):
     while True:
         if len(moves_list) > 0 and check_winner(game_field):
             comp_move(game_field, moves_list, comp)
@@ -111,9 +104,9 @@ def main():
     moves_list = [i for i in range(0, 9)]
     first = first_move(player)
     if first == 'player':
-        first_player(game_field, moves_list, player, comp)
+        first_player_move(game_field, moves_list, player, comp)
     else:
-        first_comp(game_field, moves_list, player, comp)
+        first_comp_move(game_field, moves_list, player, comp)
 
 
 if __name__ == '__main__':
